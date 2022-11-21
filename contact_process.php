d6b6ecaa5c425b911c535672092ae88d6fa4cfb7
anasraza57 <anassheikh57@gmail.com>
2022-11-21 09:37:08 -0500

    Contact Page forms created

--- contact_process.php
+++ contact_process.php
@@ -38,4 +38,118 @@ if(isset($_REQUEST['contactForm']))
 	header('Location:thank_you.html');
 	die();
 }
+
+else if(isset($_REQUEST['enquiryContact']))
+{
+    $to = "contact@emencaffe.com";
+	$from = $_REQUEST['email'];
+	$name = $_REQUEST['name'];
+	$subject = $_REQUEST['subject'];
+	$message = $_REQUEST['message'];
+
+	$headers = "From:  $from";
+	$headers = "From: " . $from . "\r\n";
+	$headers .= "Reply-To: ". $from . "\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+	$mailsubject = "You have a message by Website (Enquiries).";
+
+	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'></head><body>";
+	$body .= "<table style='width: 100%;'>";
+	$body .= "<tbody>";
+	
+	$body .= "<tr><td style='border:none;'><strong>Dear Admin,</strong></td></tr>";
+	$body .= "<tr><td style='border:none;'>You have got a new email from website. Please find the details below:</td></tr>";
+	$body .= "<tr><td style='border:none;'>&nbsp;</td></tr>";
+	$body .= "<tr><td style='border:none;'><strong>Name:</strong> {$name}</td></tr>";
+	$body .= "<tr><td style='border:none;'><strong>Email Address:</strong> {$from}</td></tr>";
+	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$subject}</td></tr>";
+	$body .= "<tr><td style='border:none;'><strong>Message:</strong> {$message}</td></tr>";
+	$body .= "<tr><td style='border:none;'>&nbsp;</td></tr>";
+	$body .= "<tr><td style='border:none;'>Thank you</td></tr>";
+	
+	$body .= "</tbody></table>";
+	$body .= "</body></html>";
+
+	$send = mail($to, $mailsubject, $body, $headers);
+	header('Location:thank_you.html');
+	die();
+}
+
+else if(isset($_REQUEST['franchiseContact']))
+{
+    $to = "contact@emencaffe.com";
+	$from = $_REQUEST['email'];
+	$name = $_REQUEST['name'];
+	$subject = $_REQUEST['subject'];
+	$message = $_REQUEST['message'];
+
+	$headers = "From:  $from";
+	$headers = "From: " . $from . "\r\n";
+	$headers .= "Reply-To: ". $from . "\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+	$mailsubject = "You have a message by Website (Franchise).";
+
+	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'></head><body>";
+	$body .= "<table style='width: 100%;'>";
+	$body .= "<tbody>";
+	
+	$body .= "<tr><td style='border:none;'><strong>Dear Admin,</strong></td></tr>";
+	$body .= "<tr><td style='border:none;'>You have got a new email from website. Please find the details below:</td></tr>";
+	$body .= "<tr><td style='border:none;'>&nbsp;</td></tr>";
+	$body .= "<tr><td style='border:none;'><strong>Name:</strong> {$name}</td></tr>";
+	$body .= "<tr><td style='border:none;'><strong>Email Address:</strong> {$from}</td></tr>";
+	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$subject}</td></tr>";
+	$body .= "<tr><td style='border:none;'><strong>Message:</strong> {$message}</td></tr>";
+	$body .= "<tr><td style='border:none;'>&nbsp;</td></tr>";
+	$body .= "<tr><td style='border:none;'>Thank you</td></tr>";
+	
+	$body .= "</tbody></table>";
+	$body .= "</body></html>";
+
+	$send = mail($to, $mailsubject, $body, $headers);
+	header('Location:thank_you.html');
+	die();
+}
+
+else if(isset($_REQUEST['brandContact']))
+{
+    $to = "contact@emencaffe.com";
+	$from = $_REQUEST['email'];
+	$name = $_REQUEST['name'];
+	$subject = $_REQUEST['subject'];
+	$message = $_REQUEST['message'];
+
+	$headers = "From:  $from";
+	$headers = "From: " . $from . "\r\n";
+	$headers .= "Reply-To: ". $from . "\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+	$mailsubject = "You have a message by Website (Brand Ambassador).";
+
+	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'></head><body>";
+	$body .= "<table style='width: 100%;'>";
+	$body .= "<tbody>";
+	
+	$body .= "<tr><td style='border:none;'><strong>Dear Admin,</strong></td></tr>";
+	$body .= "<tr><td style='border:none;'>You have got a new email from website. Please find the details below:</td></tr>";
+	$body .= "<tr><td style='border:none;'>&nbsp;</td></tr>";
+	$body .= "<tr><td style='border:none;'><strong>Name:</strong> {$name}</td></tr>";
+	$body .= "<tr><td style='border:none;'><strong>Email Address:</strong> {$from}</td></tr>";
+	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$subject}</td></tr>";
+	$body .= "<tr><td style='border:none;'><strong>Message:</strong> {$message}</td></tr>";
+	$body .= "<tr><td style='border:none;'>&nbsp;</td></tr>";
+	$body .= "<tr><td style='border:none;'>Thank you</td></tr>";
+	
+	$body .= "</tbody></table>";
+	$body .= "</body></html>";
+
+	$send = mail($to, $mailsubject, $body, $headers);
+	header('Location:thank_you.html');
+	die();
+}
 ?>
